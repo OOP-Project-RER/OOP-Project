2f80cd7c558f9b285e966005e16b5f63389c0eb7
Elend Merdzhanova <elenamerdzhanova@gmail.com>
2024-02-05 20:18:46 +0200

    create add_package

--- core/application_data.py
+++ core/application_data.py
@@ -1,6 +1,11 @@
-
-from models.scania import Trucks
+from models.trucks.trucks import Trucks
+from models.package import Package
 
 
 class ApplicationData:
-    pass
+    def __init__(self) -> None:
+        self._all_packages_list: list[Package] = []
+
+    def add_package(self, package: Package):
+        if not any(p._package_id == package._package_id for p in self._all_packages_list):
+            self._all_packages_list.append(package)
