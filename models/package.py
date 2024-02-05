6993978b4d7ba49a9e28fdaded50a43122d915bb
Elend Merdzhanova <elenamerdzhanova@gmail.com>
2024-02-05 12:30:26 +0200

    update class packege

--- models/package.py
+++ models/package.py
@@ -1,2 +1,34 @@
+from errors.application_error import ApplicationError
+from models.status import PackageStatus
+
 class Package:
-    pass
+    def __init__(self, package_id: int, start_location: str, end_location: str, package_weight: int, contact_customer: str) -> None:
+        self._package_id = package_id
+        self._start_location = start_location
+        self._end_location = end_location
+        self._package_weight = package_weight
+        self._contact_customer = contact_customer
+        self._status = PackageStatus.CREATED
+
+    @property
+    def package_id(self):
+        return self._package_id
+    
+    @property
+    def start_location(self):
+        return self._start_location
+    
+    @property
+    def end_location(self):
+        return self._end_location
+    
+    @property
+    def package_weight(self):
+        return self._package_weight
+    
+    @property
+    def contact_customer(self):
+        return self._contact_customer
+    
+    def __str__(self) -> str:
+        pass
