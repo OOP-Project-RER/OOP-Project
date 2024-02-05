8606202176ff3ed177e54bcf40341d1535264052
petrovradostin <radostin.petrov15@gmail.com>
2024-02-05 14:07:50 +0200

    updating_trucks

--- models/trucks/actros.py
+++ models/trucks/actros.py
@@ -11,6 +11,18 @@ class Actros(Trucks):
         super().__init__(name, capacity, max_range, num_of_trucks)
 
         self.name = Name.ACTROS
-        self.capacity = Actros.CAPACITY
-        self.max_range = Actros.MAX_RANGE
-        self.num_of_trucks = Actros.NUMBER_OF_TRUCKS
+        self._capacity = Actros.CAPACITY
+        self._max_range = Actros.MAX_RANGE
+        self._num_of_trucks = Actros.NUMBER_OF_TRUCKS
+
+    @property
+    def capacity(self):
+        return self._capacity
+    
+    @property
+    def max_range(self):
+        return self._max_range
+    
+    @property
+    def num_of_trucks(self):
+        return self._num_of_trucks
--- models/trucks/scania.py
+++ models/trucks/scania.py
@@ -13,3 +13,15 @@ class Scania(Trucks):
         self.capacity = Scania.CAPACITY
         self.max_range = Scania.MAX_RANGE
         self.num_of_trucks = Scania.NUMBER_OF_TRUCKS
+
+    @property
+    def capacity(self):
+        return self._capacity
+    
+    @property
+    def max_range(self):
+        return self._max_range
+    
+    @property
+    def num_of_trucks(self):
+        return self._num_of_trucks
