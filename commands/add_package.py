41da9a650eb88f6b2337d401368735ca97d96bab
Elend Merdzhanova <elenamerdzhanova@gmail.com>
2024-02-06 11:11:17 +0200

    uppdate add_packafe

--- commands/add_package.py
+++ commands/add_package.py
@@ -10,15 +10,13 @@ class AddPackageCommand(BaseCommand):
         
         self._models_factory = models_factory
         super().__init__(params, app_data)
-        #validate_params_count(params, 7)
+        validate_params_count(params, 6)
 
     @property
     def models_factory(self):
         return self._models_factory
 
     def execute(self):
-        #super().execute()
-        
         start_location_str, end_location_str, package_weight_str, *contact_customer_str = self.params
         package_weight = try_parse_float(package_weight_str)
         start_location = Locations(start_location_str)
@@ -28,7 +26,3 @@ class AddPackageCommand(BaseCommand):
         self.app_data.add_package(package)
 
         return f'Package #{package.package_id} with weight {package_weight} was added to the waiting list.'
-    
-    def _expected_params_count(self) -> int:
-        return 3
-
