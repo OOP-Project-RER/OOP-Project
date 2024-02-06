2b84aebee123a667f54201d755356e1e4ddc378d
romadimitrov <rdimitrov877@gmail.com>
2024-02-06 10:57:46 +0200

    fix addPackageCommand

--- commands/add_package.py
+++ commands/add_package.py
@@ -7,22 +7,28 @@ from models.constants.locations import Locations
 
 class AddPackageCommand(BaseCommand):
     def __init__(self, params: list[str], app_data: ApplicationData, models_factory: ModelsFactory):
-        super().__init__(params, app_data)
-        validate_params_count(params, 7)
+        
         self._models_factory = models_factory
+        super().__init__(params, app_data)
+        #validate_params_count(params, 7)
 
     @property
     def models_factory(self):
         return self._models_factory
 
     def execute(self):
-        package_id_str, start_location_str, end_location_str, package_weight_str, *contact_customer_str = self._params
-        package_id = try_parse_int(package_id_str)
+        #super().execute()
+        
+        start_location_str, end_location_str, package_weight_str, *contact_customer_str = self.params
         package_weight = try_parse_float(package_weight_str)
         start_location = Locations(start_location_str)
         end_location = Locations(end_location_str)
         contact_customer = Customer(*contact_customer_str)
-        package = self.models_factory.create_package(package_id, start_location, end_location, package_weight, contact_customer)
+        package = self.models_factory.create_package(start_location, end_location, package_weight, contact_customer)
         self.app_data.add_package(package)
 
-        return f'Package #{package_id} was added to the waiting list.'
+        return f'Package #{package.package_id} with weight {package_weight} was added to the waiting list.'
+    
+    def _expected_params_count(self) -> int:
+        return 3
+
